2037b19efc2fad9a9c38d8a45a8532082ea0199a
trkulja-jovan <jovantrkulja9@hotmail.com>
2020-12-04 22:57:20 +0100

    added home page

--- UI/PlannerMob/PlannerMob/App.xaml.cs
+++ UI/PlannerMob/PlannerMob/App.xaml.cs
@@ -4,16 +4,14 @@ namespace PlannerMob
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
-
         protected override void OnStart()
         {
+            Shell.Current.GoToAsync("//homePage");
         }
 
         protected override void OnSleep()
--- UI/PlannerMob/PlannerMob/AppShell.xaml.cs
+++ UI/PlannerMob/PlannerMob/AppShell.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace PlannerMob
+﻿using Xamarin.Forms;
+
+namespace PlannerMob
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
+        }
+
+        private async void MenuItem_Clicked(object sender, System.EventArgs e)
+        {
+            await Shell.Current.DisplayAlert("Title", "Poruka", "Ok");
         }
     }
 }
